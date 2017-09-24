772e794e092e94b9091972d1aca9e2638ccf5953
Bonggyun Lee <deptno@gmail.com>
2017-09-25 01:34:18 +0900

    러나 실패 느낌

--- pages/authorized/github.tsx
+++ pages/authorized/github.tsx
@@ -2,14 +2,14 @@ import * as React from 'react'
 import {Provider, Store} from 'react-redux'
 import {StaticPage} from '../_page'
 import {Layout} from '../../src/components/Layout'
-import {isomorphicQuery} from 'nextjs-isomorphic-query'
+import {isomorphicQuery} from '../../packages/nextjs-isomorphic-query'
 
 export default class Index extends StaticPage<{}> {
   render() {
     return (
       <Provider store={this.store}>
         <Layout>
-          {JSON.stringify(this.props.url.query, null, 4)}
+          {JSON.stringify(isomorphicQuery(this.props.url.query), null, 4)}
         </Layout>
       </Provider>
     )
